40eea6b27d1f90acfbf42db45f80174ae57a0b94
exaterina <leni.snow@yandex.ru>
2022-06-25 15:48:43 +0200

    -

--- main.pl
+++ main.pl
@@ -1,12 +1,18 @@
-main:- 
+:- use_module(library(readutil)).
+
+
+% Read a map from "map.txt"
+
+read(Map):- 
     open("map.txt", read, Str), 
     read_map(Str, Map), 
     close(Str), 
+    format('~n~n~n   Game will be played on a map: ~d ~n~n~n', Map),
     write(Map),  nl. 
     
 read_map(Stream, []):- at_end_of_stream(Stream). 
     
 read_map(Stream, [X | L]):- 
     \+ at_end_of_stream(Stream), 
-    read(Stream, X), 
+    read_line_to_string(Stream, X), 
     read_map(Stream, L).
